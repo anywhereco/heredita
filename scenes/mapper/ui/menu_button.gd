856c9963a864fe8aaa194a0ee87eec8dba6d843a
Whirling <blazem7708.2@gmail.com>
2026-09-21 21:08:04 -0600

    feature: add ui keybinds (#53)
    * feat: z/x/c keybinds for marking tabs
    
    * feat: also add esc keybind. yea

--- scenes/mapper/ui/menu_button.gd
+++ scenes/mapper/ui/menu_button.gd
@@ -1,9 +1,21 @@
+class_name UIMenuButton
 extends Button
 
+var pause_menu_open: bool = false
 
 func _pressed() -> void:
+	open_pause_menu()
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause_menu"):
+		open_pause_menu()
+
+func open_pause_menu() -> void:
+	if pause_menu_open:
+		return
 	var prompt_res := Prompts.new_fullscreen_prompt()
 	if prompt_res.is_ok():
+		pause_menu_open = true
 		var prompt: PromptInstance = prompt_res.val()
 		prompt.hide_panel()
 		var menu := preload("res://scenes/mapper/ui/pause_menu/menu.tscn").instantiate()
