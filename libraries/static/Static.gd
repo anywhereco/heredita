69b7abef8bfc0a04791934b10d115c6670ab3342
Whirling <infernity@infernity.dev>
2026-09-22 16:35:22 -0600

    Add server bridge keychecking

--- libraries/static/Static.gd
+++ libraries/static/Static.gd
@@ -4,6 +4,8 @@ extends Object
 
 static var HEREDITA_URL := "https://app.heredita.net"
 static var SERVER_URL := "wss://gameserver.heredita.net"
+## May be empty
+static var S2S_KEY := ""
 
 const CHUNK_SIZE: int = 256
 const CHUNK_SIZE_FLOAT: float = float(CHUNK_SIZE)
@@ -14,8 +16,11 @@ static func initialize() -> void:
 		HEREDITA_URL = "http://127.0.0.1:9000"
 		SERVER_URL = "ws://127.0.0.1:9001"
 	var user_args := OS.get_cmdline_user_args()
+	S2S_KEY = OS.get_environment("HEREDITA_S2S_KEY")
 	for i in range(user_args.size()):
 		if user_args[i].contains("--heredita-url="):
 			HEREDITA_URL = user_args[i].split("=")[1]
 		if user_args[i].contains("--gameserver-url="):
 			SERVER_URL = user_args[i].split("=")[1]
+		if user_args[i].contains("--s2s-key="):
+			S2S_KEY = user_args[i].split("=")[1]
